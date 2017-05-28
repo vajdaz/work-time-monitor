304a8d0f59f164c476c78fcfe3bdfecae5bedb8c
Zoltán Vajda <vzvzvz2@gmail.com>
2017-05-28 21:24:29 +0200

    Tiny refactorings

--- src/vajdaz/WorkTimeMonitor.java
+++ src/vajdaz/WorkTimeMonitor.java
@@ -1,5 +1,6 @@
 package vajdaz;
 
+import java.awt.AWTException;
 import java.awt.Image;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
@@ -20,9 +21,22 @@ import vajdaz.util.PersistentProperties;
 
 public class WorkTimeMonitor {
 	private static final String CONFIG_FILENAME = "vajdaz.WorktimeMonitor.config";
-	private Image image1 = null;
-	private Image image2 = null;
-	private Image image3 = null;
+	private static final Image imge_red;
+	private static final Image image_red_yellow;
+	private static final Image image_green;
+	static {
+		try {
+			imge_red = javax.imageio.ImageIO
+					.read(WorkTimeMonitor.class.getClassLoader().getResource("traffic_light_red.png"));
+			image_red_yellow = javax.imageio.ImageIO
+					.read(WorkTimeMonitor.class.getClassLoader().getResource("traffic_light_red_yellow.png"));
+			image_green = javax.imageio.ImageIO
+					.read(WorkTimeMonitor.class.getClassLoader().getResource("traffic_light_green.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error: could not load icon images.");
+		}
+	}
 	private TrayIcon trayIcon = null;
 	private PersistentProperties props = new WorkTimeMonitorProperties(CONFIG_FILENAME);
 	private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
@@ -32,19 +46,19 @@ public class WorkTimeMonitor {
 			throw new RuntimeException("Error: Systray not supported.");
 		}
 		try {
-			image1 = javax.imageio.ImageIO.read(WorkTimeMonitor.class.getClassLoader().getResource("ampel_1.png"));
-			image2 = javax.imageio.ImageIO.read(WorkTimeMonitor.class.getClassLoader().getResource("ampel_2.png"));
-			image3 = javax.imageio.ImageIO.read(WorkTimeMonitor.class.getClassLoader().getResource("ampel_3.png"));
-			trayIcon = new TrayIcon(image1, "", createMenu());
+			trayIcon = new TrayIcon(imge_red, "", createMenu());
 			trayIcon.setImageAutoSize(true);
 			SystemTray.getSystemTray().add(trayIcon);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+		} catch (AWTException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error: could not initialize tray icon.");
 		}
 	}
 
 	private PopupMenu createMenu() {
+		// Root popup menu when right clicking the icon
 		PopupMenu popup = new PopupMenu();
+		// Menu item for closing the application
 		MenuItem menuItem = new MenuItem("Beenden");
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
@@ -59,6 +73,7 @@ public class WorkTimeMonitor {
 			}
 		});
 		popup.add(menuItem);
+		// Menu item for reloading configuration data
 		menuItem = new MenuItem("Einstellungen neu laden");
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
@@ -67,7 +82,8 @@ public class WorkTimeMonitor {
 			}
 		});
 		popup.add(menuItem);
-		menuItem = new MenuItem("Konfigurationsdatei öffnen");
+		// Menu item for opening configuration file
+		menuItem = new MenuItem("Konfigurationsdatei \u00F6ffnen");
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				try {
@@ -84,8 +100,8 @@ public class WorkTimeMonitor {
 			}
 		});
 		popup.add(menuItem);
-		popup.add(menuItem);
-		menuItem = new MenuItem("Pausenzeit zurücksetzen");
+		// Menu item for resetting the break time
+		menuItem = new MenuItem("Pausenzeit zur\u00FCcksetzen");
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				WorkTimeMonitor.this.resetBreakTime();
@@ -110,8 +126,9 @@ public class WorkTimeMonitor {
 			return false;
 		}
 		Calendar calNow = Calendar.getInstance();
-		return (calNow.get(1) == calLastRun.get(1)) && (calNow.get(2) == calLastRun.get(2))
-				&& (calNow.get(5) == calLastRun.get(5));
+		return (calNow.get(Calendar.YEAR) == calLastRun.get(Calendar.YEAR))
+				&& (calNow.get(Calendar.MONTH) == calLastRun.get(Calendar.MONTH))
+				&& (calNow.get(Calendar.DAY_OF_MONTH) == calLastRun.get(Calendar.DAY_OF_MONTH));
 	}
 
 	private void startup() {
@@ -146,7 +163,7 @@ public class WorkTimeMonitor {
 		Date now = new Date();
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(now);
-		cal.add(10, 1);
+		cal.add(Calendar.HOUR_OF_DAY, 1);
 		props.setPropertyPersistent("breakTime", new SimpleDateFormat().format(cal.getTime()));
 		return cal.getTimeInMillis();
 	}
@@ -163,7 +180,7 @@ public class WorkTimeMonitor {
 		if (now.getTime() > breakTime.getTime()) {
 			Calendar cal = Calendar.getInstance();
 			cal.setTime(now);
-			cal.add(10, 1);
+			cal.add(Calendar.HOUR_OF_DAY, 1);
 			props.setPropertyPersistent("breakTime", new SimpleDateFormat().format(cal.getTime()));
 			return true;
 		}
@@ -183,15 +200,15 @@ public class WorkTimeMonitor {
 		long netWorktimeDone = grossWorkitmeDone - lunchime;
 		long netWorktime = (long) (3600.0 * Double.parseDouble(props.getProperty("worktimeHours", "8")));
 
-		if ((netWorktimeDone <= netWorktime - 3600) && (trayIcon.getImage() != image1)) {
-			trayIcon.setImage(image1);
+		if ((netWorktimeDone <= netWorktime - 3600) && (trayIcon.getImage() != imge_red)) {
+			trayIcon.setImage(imge_red);
 		} else if ((netWorktimeDone > netWorktime - 3600) && (netWorktimeDone <= netWorktime)
-				&& (trayIcon.getImage() != image2)) {
-			trayIcon.setImage(image2);
+				&& (trayIcon.getImage() != image_red_yellow)) {
+			trayIcon.setImage(image_red_yellow);
 			trayIcon.displayMessage("Worktime Monitor", "Noch eine Stunde", TrayIcon.MessageType.INFO);
 			Toolkit.getDefaultToolkit().beep();
-		} else if ((netWorktimeDone > netWorktime) && (trayIcon.getImage() != image3)) {
-			trayIcon.setImage(image3);
+		} else if ((netWorktimeDone > netWorktime) && (trayIcon.getImage() != image_green)) {
+			trayIcon.setImage(image_green);
 			trayIcon.displayMessage("Worktime Monitor", "Go, go, go!", TrayIcon.MessageType.INFO);
 			Toolkit.getDefaultToolkit().beep();
 		}
@@ -211,10 +228,10 @@ public class WorkTimeMonitor {
 		String sTimeAll = String.format("Netto Arbeit: " + (split[0] < 0 ? "-" : "") + "%d:%02d",
 				new Object[] { Long.valueOf(split[2]), Long.valueOf(split[3]) });
 		split = HelperFunctions.splitTimeDiff(netWorktime - netWorktimeDone);
-		String sTimeRemaining = String.format((split[0] < 0 ? "Überzug " : "Restzeit: ") + "%d:%02d",
+		String sTimeRemaining = String.format((split[0] < 0 ? "\u00DCberzug " : "Restzeit: ") + "%d:%02d",
 				new Object[] { Long.valueOf(split[2]), Long.valueOf(split[3]) });
 		split = HelperFunctions.splitTimeDiff(nextBreakInSeconds - now.getTime() / 1000);
-		String sNextBreak = String.format(split[0] < 0 ? "Nächste Pause: -" : "Nächste Pause: %d:%02d",
+		String sNextBreak = String.format(split[0] < 0 ? "N\u00E4chste Pause: -" : "N\u00E4chste Pause: %d:%02d",
 				new Object[] { Long.valueOf(split[2]), Long.valueOf(split[3]) });
 		split = HelperFunctions.splitTimeDiff(lunchime);
 		String sLunchTime = String.format("Mittagspause: %d:%02d",
